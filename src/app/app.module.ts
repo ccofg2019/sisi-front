29754b710e54d224bc35fd882bbd711d0c84f9a8
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2018-10-09 12:51:42 -0300

    01.01 - feature/frt-0003 - component form register + css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
