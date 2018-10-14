b397f9d00152a53e70c45293ba96a322656ca770
Wagner Quintas <wagnerquintas@outlook.com>
2018-10-14 15:11:41 -0300

    01.01 - feature/frt-0009 - tela home + mapa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let L;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,10 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    const map = L.map('map').setView([-8.05225025, -34.9450490084884], 17);
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 }
