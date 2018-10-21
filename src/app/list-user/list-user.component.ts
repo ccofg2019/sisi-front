e15538bfc91cedd9109a2c6dfcfee5a124c49ed8
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2018-10-21 20:12:24 -0300

    02.01-feature/frt-0010-paginação de usuários (faltando integrar html + componente)

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -1,4 +1,4 @@
-import { User } from './../models/user.model';
+import { User, Page, Links } from './../models/user.model';
 import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -12,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
 export class ListUserComponent implements OnInit {
 
   public users: User[];
+  public page: Page;
+
+  numPage: any;
+
+  
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((response: any) => this.users = response.data);
+    //this.userService.getUsers().subscribe((response: any) => this.users = response.data);
+    this.pageUsers(5);
+    console.log(this.numPage);
   }
+
+  pageUsers(page){
+    this.userService.getUsersPage(page)
+    .subscribe((response: any) => {
+      this.page = response.pagination
+      this.users = response.data; 
+      
+    } );
+  }
+
 }
