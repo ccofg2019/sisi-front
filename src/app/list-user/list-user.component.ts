aacda96147434f247a02a13540b3480367a8318a
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2018-10-22 01:13:33 -0300

    02.01- feature/frt-0010 - sistema de paginação dinâmico (version beta)

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -13,26 +13,49 @@ export class ListUserComponent implements OnInit {
 
   public users: User[];
   public page: Page;
+  public TotalPage;
 
-  numPage: any;
-
+  numPage = 1;
   
-
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     //this.userService.getUsers().subscribe((response: any) => this.users = response.data);
-    this.pageUsers(5);
-    console.log(this.numPage);
+    this.pageUsers(1);
   }
 
+  //BUSCANDO AS INFORMAÇÕES DA TABELA E AS INFORMAÇÕES DA PAGINAÇÃO
   pageUsers(page){
     this.userService.getUsersPage(page)
     .subscribe((response: any) => {
       this.page = response.pagination
-      this.users = response.data; 
-      
+      this.users = response.data
+      this.TotalPage = response.meta.pagination.total_pages
+      this.BuscarMaxPage(this.TotalPage);
     } );
   }
+
+  BuscarMaxPage(pageMax){
+    pageMax = this.TotalPage;
+    return this.TotalPage;
+  }
+
+  nextPage(){
+    if(this.numPage < this.TotalPage){
+      this.numPage = this.numPage +1;
+      this.pageUsers(this.numPage);
+      console.log(this.numPage);
+    }
+  }
+
+  previousPage(){
+    if(this.numPage != 1){
+      this.numPage = this.numPage -1;
+      this.pageUsers(this.numPage);
+      console.log(this.numPage);
+    }
+  }
+  
+ 
 
 }
